374b248d9f116cf1f51f03240e5cce3d1355bbd5
mcowellfau <mcowell2020@fau.edu>
2023-09-16 11:29:35 -0400

    created a player.py file and initialized a player class

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CAP4630_tic_tac_toe_Fall2023
+
---
+++ player.py
@@ -0,0 +1,7 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        #letter is either x or o
+        self.letter = letter
